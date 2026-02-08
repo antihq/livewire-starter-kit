22722ae756bf4a057e4b625e032125f3f3403250
Oliver Servín <7003648+oliverservin@users.noreply.github.com>
2026-02-07 20:54:20 -0600

    core: allow members to post listings in the marketplace

--- app/Providers/JetstreamServiceProvider.php
+++ app/Providers/JetstreamServiceProvider.php
@@ -57,5 +57,7 @@ class JetstreamServiceProvider extends ServiceProvider
             'create',
             'update',
         ])->description('Editor users have the ability to read, create, and update.');
+
+        Jetstream::role('member', 'Member', [])->description('Member users have the ability to post listings.');
     }
 }
